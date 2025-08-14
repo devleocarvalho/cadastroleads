11eac5f6159a5490dc0f2abeb9905dc3ee31da9c
kleber de carvalho <dev.leocarvalho@gmail.com>
2025-08-14 17:32:25 -0300

    inicio do projeto

---
+++ script.js
@@ -0,0 +1,35 @@
+document.getElementById('form-consumidor').addEventListener('submit', async function(event) {
+    event.preventDefault();
+
+    const nome = document.getElementById('consumidor-nome').value;
+    const telefone = document.getElementById('consumidor-telefone').value;
+
+    const dadosLead = {
+        nome: nome,
+        telefone: telefone
+    };
+
+    try {
+        const resposta = await fetch('https://vxyctdclkfasiakasjvr.supabase.co/rest/v1/Leads', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ4eWN0ZGNsa2Zhc2lha2FzanZyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUxODEwMDksImV4cCI6MjA3MDc1NzAwOX0.R1WbyrzEGJxmg1q7oSSwF1_vpjN3lnQn4S247LBF0w0'
+            },
+            body: JSON.stringify(dadosLead)
+        });
+
+        if (resposta.ok) {
+            console.log('Lead cadastrado com sucesso!');
+            document.getElementById('form-consumidor').reset();
+            alert('Lead cadastrado com sucesso!');
+        } else {
+            const erro = await resposta.json();
+            console.error('Erro ao cadastrar o lead:', erro);
+            alert('Erro ao cadastrar o lead. Por favor, tente novamente.');
+        }
+    } catch (error) {
+        console.error('Erro na requisição:', error);
+        alert('Erro na conexão. Verifique sua rede e tente novamente.');
+    }
+});
